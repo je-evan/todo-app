c1a5b1ec3d86b79c7925479f2b7d1e7ff8f0348b
je-evan <dhakaljeevan165@gmail.com>
2021-06-02 14:19:27 +0545

    update settings

--- to_do_list/settings.py
+++ to_do_list/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # SECURITY WARNING: keep the secret key used in production secret!
 # SECRET_KEY = '$$2uel1$@&e#z(g1w3ry6%pzakq*o^97)kr(alotjhv(ot%5l6'
 
-SECRET_KEY = SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', '')
+SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', '')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 # DEBUG = True
